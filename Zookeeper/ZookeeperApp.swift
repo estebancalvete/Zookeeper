da8e4fb509cfae9e3fe27540c564b00debee361b
Esteban Calvete <esteban.calvete@gmail.com>
2022-04-14 12:54:27 +0200

    Initial Commit

---
+++ Zookeeper/ZookeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeeperApp.swift
+//  Zookeeper
+//
+//  Created by Esteban Calvete Iglesias on 14/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ZookeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
